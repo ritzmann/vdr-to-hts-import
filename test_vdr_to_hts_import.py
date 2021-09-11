879eb9bbbb04a55765f7e3b3dd7295a81b4edf38
Fabian Ritzmann <ritzmann@users.noreply.github.com>
2021-09-11 20:00:55 +0300

    More tests

--- test_vdr_to_hts_import.py
+++ test_vdr_to_hts_import.py
@@ -10,6 +10,13 @@ def test_get_channel_name():
     assert 'channel1' == info.get_channel_name()
 
 
+def test_get_channel_name_with_multiple_spaces():
+    info = Info('test')
+    info.info['C'] = 'some-id name of channel 1'
+
+    assert 'name of channel 1' == info.get_channel_name()
+
+
 def test_get_channel_name_no_id():
     info = Info('test')
     info.info['C'] = 'channel1'
