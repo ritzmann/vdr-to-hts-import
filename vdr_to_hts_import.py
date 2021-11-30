a9bde1b011258228d0689a7712faf1d028b51fad
Fabian Ritzmann <ritzmann@users.noreply.github.com>
2021-12-01 00:29:07 +0200

    Include subtitles when concatenating video files

--- vdr_to_hts_import.py
+++ vdr_to_hts_import.py
@@ -223,7 +223,8 @@ class Config:
             for file in files:
                 concat_files.write("file '" + str(self.directory / file) + "'\n")
         filename = self.directory / 'concat.ts'
-        subprocess.run(['ffmpeg', '-f', 'concat', '-safe', '0', '-i', str(filelist_path), '-c', 'copy', str(filename)],
+        subprocess.run(['ffmpeg', '-f', 'concat', '-safe', '0', '-i', str(filelist_path), '-c', 'copy', '-scodec', 'copy', 
+                        str(filename)],
                        check=True, text=True)
         return filename
 
